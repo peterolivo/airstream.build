cb557da6e4d40702987b57517d5d38d32a8e3ace
Peter Olivo <peter.olivo@savvis.com>
2014-10-17 15:39:32 -0500

    update to plugins

--- wp-config.php
+++ wp-config.php
@@ -4,18 +4,26 @@ $services_json = json_decode($services,true);
 $mysql_config = $services_json["mysql-5.1"][0]["credentials"];
 
 // ** MySQL settings from resource descriptor ** //
-define('DB_NAME', $mysql_config["name"]);
-define('DB_USER', $mysql_config["user"]);
-define('DB_PASSWORD', $mysql_config["password"]);
-define('DB_HOST', $mysql_config["hostname"]);
-define('DB_PORT', $mysql_config["port"]);
-
+if (getenv("VCAP_SERVICES")) {
+	define('DB_NAME', $mysql_config["name"]);
+	define('DB_USER', $mysql_config["user"]);
+	define('DB_PASSWORD', $mysql_config["password"]);
+	define('DB_HOST', $mysql_config["hostname"]);
+	define('DB_PORT', $mysql_config["port"]);
+	//define('WP_HOME', 'http://airstream.uc01.clc.af.cm/');
+	//define('WP_SITEURL', 'http://www.airstream.build/');
+} else {
+	define('DB_NAME', 'airstreamWordpress');
+	define('DB_USER', 'root');
+	define('DB_PASSWORD', 'root');
+	define('DB_HOST', 'localhost');
+	define('WP_HOME', 'http://localhost');
+	define('WP_SITEURL', 'http://localhost');
+}
 define('DB_CHARSET', 'utf8');
 define('DB_COLLATE', '');
 define ('WPLANG', '');
 define('WP_DEBUG', false);
-
-require('wp-salt.php');
 
 $table_prefix  = 'wp_';
 
