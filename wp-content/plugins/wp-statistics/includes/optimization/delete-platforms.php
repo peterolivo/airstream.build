73ece55b314682a774b493a6ca941a4eea285b71
Peter Olivo <peter.olivo@savvis.com>
2014-10-23 14:41:50 -0500

    added wp-statistics

---
+++ wp-content/plugins/wp-statistics/includes/optimization/delete-platforms.php
@@ -0,0 +1,23 @@
+﻿<?php
+	require('../../../../../wp-blog-header.php');
+	
+	if( !is_super_admin() )
+		wp_die(__('Access denied!', 'wp_statistics'));
+		
+	$platform = $_POST['platform_name'];
+	
+	if($platform) {
+		
+		$result = $wpdb->query("DELETE FROM {$table_prefix}statistics_visitor WHERE platform = '$platform'");
+		
+		if($result) {
+			echo sprintf(__('%s platform data deleted successfully.', 'wp_statistics'), '<code>' . $platform . '</code>');
+		}
+		else {
+			_e('No platform data found to remove!', 'wp_statistics');
+		}
+		
+	} else {
+		_e('Please select the desired items.', 'wp_statistics');
+	}
+?>
